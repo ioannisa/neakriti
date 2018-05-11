99508367dcff9ecf37aa451af784a4ae039fd5ef
ioannisa <ioannisanif@gmail.com>
2018-05-11 16:00:18 +0300

    Corrected XML Model for images (thumb and large icons)

---
+++ app/src/main/java/eu/anifantakis/neakriti/data/model/RssImgThumb.java
@@ -0,0 +1,29 @@
+package eu.anifantakis.neakriti.data.model;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
+@Root(name = "img_thumb", strict = false)
+public class RssImgThumb {
+
+    @Attribute
+    private String url;
+
+    @Attribute
+    private String length;
+
+    @Attribute
+    private String type;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLength(){
+        return length;
+    }
+
+    public String getType(){
+        return type;
+    }
+}
