6a3cc7af49bfe34349e2331b1c1d419ba4583fd4
ioannisa <ioannisanif@gmail.com>
2018-06-04 22:29:31 +0300

    Splash Screen is made "Full Screen"

--- app/src/main/java/eu/anifantakis/neakriti/SplashActivity.java
+++ app/src/main/java/eu/anifantakis/neakriti/SplashActivity.java
@@ -1,8 +1,8 @@
 package eu.anifantakis.neakriti;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
