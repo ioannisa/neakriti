40ca4e28a46334bdf5525e3ebd893a08b63f9961
ioannisa <ioannisanif@gmail.com>
2018-09-04 16:55:42 +0300

    CSS for better article display, facebook comments

--- app/src/main/java/eu/anifantakis/neakriti/ArticleDetailFragment.java
+++ app/src/main/java/eu/anifantakis/neakriti/ArticleDetailFragment.java
@@ -8,8 +8,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
@@ -25,10 +28,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.CookieManager;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.analytics.HitBuilders;
@@ -45,7 +56,9 @@ import eu.anifantakis.neakriti.databinding.FragmentArticleDetailBinding;
 import eu.anifantakis.neakriti.utils.AppUtils;
 import eu.anifantakis.neakriti.utils.NeaKritiApp;
 
+import static eu.anifantakis.neakriti.utils.AppUtils.isNetworkAvailable;
 import static eu.anifantakis.neakriti.utils.AppUtils.isNightMode;
+import static eu.anifantakis.neakriti.utils.AppUtils.onlineMode;
 import static eu.anifantakis.neakriti.utils.NeaKritiApp.sharedPreferences;
 
 /**
@@ -63,6 +76,10 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
     private WebView mWebView;
     private AdView adView;
     private CollapsingToolbarLayout appBarLayout;
+
+    private FrameLayout mContainer;
+    private WebView mWebViewComments;
+    private WebView mWebviewPop;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -123,21 +140,57 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
         TextView detailDate = binding.detailDate;
         detailDate.setText(AppUtils.pubDateFormat(mArticle.getPubDateStr()));
 
-        mWebView = binding.articleDetail;
-        webSettings = mWebView.getSettings();
+        // comments section
+        mWebViewComments = binding.commentsView;
+        mContainer = binding.webviewFrame;
 
+        // article section
+        mWebView = binding.articleDetail;
+
+        webSettings = mWebView.getSettings();
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setDatabaseEnabled(true);
         webSettings.setDomStorageEnabled(true);
+
         mWebView.getSettings().setAllowFileAccess(true);
         webSettings.setAppCacheEnabled(true);
 
+        CookieManager.getInstance().setAcceptCookie(true);
+        if (Build.VERSION.SDK_INT >= 21) {
+            mWebView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
+        }
+
         // set the webview scrollbar to be "inside" to avoid unecessary "right-side" padding space
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                // Once the article is done loading, load the comments section if we are in online mode
+                if (onlineMode){
+                    loadComments();
+                }
+            }
+        });
 
         adView = binding.adView;
+
+        adView.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+
+                // This is a working hack...
+                // The advert loading causes undesired scrolling.  To solve this we disable focusable descendants in xml
+                // however disabling focusable descendants, does not allow to focus on the facebook comments section and write your comment.
+                // so when the advert is done displaying the advert, its now safe to re-enable focusable descendants without undesirable scrolls.
+                binding.articleContainer.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+            }
+        });
 
         // Handling Rotation
         if (savedInstanceState!=null){
@@ -161,6 +214,9 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
     }
 
     private void displayArticle(){
+        // update possible online mode change
+        onlineMode = isNetworkAvailable(getContext());
+
         if (sharedPreferences == null) {
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         }
@@ -177,16 +233,34 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
 
         String dayNightStyle = "";
         if (isNightMode){
-            dayNightStyle = "body,p,div{background:#333333 !Important; color:#eeeeee;} a{color:#ee3333 !Important}";
+            dayNightStyle =
+                    "body,p,div{background:#333333 !Important; color:#eeeeee;} a{color:#ee3333 !Important}" +
+                    "blockquote p {background:#444 !Important} "+
+                    "blockquote{background:#444 !Important; border-left: 10px solid #666 !Important;} "+
+                    "blockquote:before { color:#666 !Important }"+
+
+                    ".descr p {background:#444 !Important} "+
+                    ".descr{color: #ccc; background:#444 !Important; border-left: 10px solid #666 !Important;} "+
+                    ".descr:before { color:#666 !Important }";
             //binding.articleNestedScrollView.setBackgroundColor(Color.parseColor("#333333"));
         }
+
+        String standardStyle =
+                        ".video-container{position: relative;padding-bottom: 56.25%;margin-top:20px;height: 0;overflow: hidden;}.video-container iframe, .video-container object, .video-container embed{position: absolute;top: 0;left: 0;width: 100%;height: 100%;}figure{margin-bottom:0;margin-left:0;margin-right:0;padding:0;width=\"100%\"}img{width: 100%;}"+
+
+                        " .descr {background: #eee !Important; color:#444; font-size:0.9em; border-left: 10px solid #ccc; margin: 1.5em 00px; padding: 0.5em 10px;}" +
+                        " .descr:before {color: #ccc; line-height: 0.1em; margin-right: 0.25em; vertical-align: -0.4em;}" +
+
+                        " blockquote {background: #eee;border-left: 10px solid #ccc; margin: 1.5em 10px; padding: 0.5em 10px; quotes: '\\201C''\\201D''\\2018''\\2019'}" +
+                        " blockquote:before {color: #ccc; content: open-quote; font-size: 4em; line-height: 0.1em; margin-right: 0.25em; vertical-align: -0.4em;}" +
+                        " blockquote p {display: inline;} ";
 
         String webStory =
                 "<!DOCTYPE html><html lang=\"el\"><head><title>{0}</title> "+
                 "<meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\"/>"+
                 "<link rel=\"canonical\" href=\"{1}\"> <link href=\"https://fonts.googleapis.com/css?family=Roboto:400,700,900&subset=greek,latin\" rel=\"stylesheet\" type=\"text/css\"> "+
                 "<style> a'{' text-decoration:none; color:#a00; font-weight:600; '}' body'{'line-height:normal; font-family:\"Roboto\"; padding-bottom:50px;{2}'}' "+
-                "object,img,iframe,div,video,param,embed'{'max-width: 100%; '}'{3}</style></head><body>{4}</body></html>";
+                "object,img,iframe,div,video,param,embed'{'max-width: 100%; '}'{3}{4}</style></head><body>{5}</body></html>";
 
         String format;
         if (sharedPreferences.getBoolean(getString(R.string.pref_increased_line_distance_key), true)){
@@ -196,12 +270,15 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
             format = "";
         }
 
-        webStory = MessageFormat.format(webStory, mArticle.getTitle(), mArticle.getLink(), format, dayNightStyle, articleDetail);
-        mWebView.loadDataWithBaseURL(null, webStory, "text/html", "utf-8", null);
+        webStory = MessageFormat.format(webStory, mArticle.getTitle(), mArticle.getLink(), format, standardStyle, dayNightStyle, articleDetail);
+        mWebView.loadDataWithBaseURL(mArticle.getLink(), webStory, "text/html", "utf-8", null);
 
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
-        adView.loadAd(adRequest);
+        // show adverts if we are online
+        if (onlineMode) {
+            AdRequest adRequest = new AdRequest.Builder()
+                    .setRequestAgent("android_studio:ad_template").build();
+            adView.loadAd(adRequest);
+        }
     }
 
     /**
@@ -486,6 +563,102 @@ public class ArticleDetailFragment extends Fragment implements TextToSpeech.OnIn
                 mTextToSpeech.stop();
             }
             mTextToSpeech.shutdown();
+        }
+    }
+
+    /**
+     * Load the facebook comments for the displayed article
+     * Source: https://www.androidhive.info/2016/09/android-adding-facebook-comments-widget-in-app/
+     */
+    private void loadComments() {
+        mWebViewComments.setWebViewClient(new UriWebViewClient());
+        mWebViewComments.setWebChromeClient(new UriChromeClient());
+        mWebViewComments.getSettings().setJavaScriptEnabled(true);
+        mWebViewComments.getSettings().setAppCacheEnabled(true);
+        mWebViewComments.getSettings().setDomStorageEnabled(true);
+        mWebViewComments.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebViewComments.getSettings().setSupportMultipleWindows(true);
+        mWebViewComments.getSettings().setSupportZoom(false);
+        mWebViewComments.getSettings().setBuiltInZoomControls(false);
+        CookieManager.getInstance().setAcceptCookie(true);
+        if (Build.VERSION.SDK_INT >= 21) {
+            mWebViewComments.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebViewComments, true);
+        }
+
+        // facebook comment widget including the article url
+        String facebookComments =
+                " <!doctype html> <html lang='el'> <head></head> <body> " +
+                    " <div id='fb-root'></div>" +
+                    " <script>(function(d, s, id) {" +
+                    "  var js, fjs = d.getElementsByTagName(s)[0];" +
+                    "  if (d.getElementById(id)) return;" +
+                    "  js = d.createElement(s); js.id = id;" +
+                    "  js.src = 'https://connect.facebook.net/el_GR/sdk.js#xfbml=1&version=v3.1&appId=56856387271&autoLogAppEvents=1';" +
+                    "  fjs.parentNode.insertBefore(js, fjs);" +
+                    "}(document, 'script', 'facebook-jssdk'));</script>" +
+
+                    " <div class='fb-comments' data-href='" + mArticle.getLink() + "' data-num-posts='30'></div> " +
+                " </body> </html>";
+
+
+        mWebViewComments.loadDataWithBaseURL("https://www.neakriti.gr", facebookComments, "text/html", "UTF-8", null);
+        mWebViewComments.setMinimumHeight(200);
+    }
+
+
+    private class UriWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            String host = Uri.parse(url).getHost();
+            return !host.equals("m.facebook.com");
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            String host = Uri.parse(url).getHost();
+            if (url.contains("/plugins/close_popup.php?reload")) {
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Do something after 100ms
+                        mContainer.removeView(mWebviewPop);
+                        loadComments();
+                    }
+                }, 600);
+            }
+        }
+    }
+
+    class UriChromeClient extends WebChromeClient {
+
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog,
+                                      boolean isUserGesture, Message resultMsg) {
+            mWebviewPop = new WebView(getContext());
+            mWebviewPop.setVerticalScrollBarEnabled(false);
+            mWebviewPop.setHorizontalScrollBarEnabled(false);
+            mWebviewPop.setWebViewClient(new UriWebViewClient());
+            mWebviewPop.setWebChromeClient(this);
+            mWebviewPop.getSettings().setJavaScriptEnabled(true);
+            mWebviewPop.getSettings().setDomStorageEnabled(true);
+            mWebviewPop.getSettings().setSupportZoom(false);
+            mWebviewPop.getSettings().setBuiltInZoomControls(false);
+            mWebviewPop.getSettings().setSupportMultipleWindows(true);
+            mWebviewPop.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+            mContainer.addView(mWebviewPop);
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(mWebviewPop);
+            resultMsg.sendToTarget();
+
+            return true;
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
         }
     }
 }
